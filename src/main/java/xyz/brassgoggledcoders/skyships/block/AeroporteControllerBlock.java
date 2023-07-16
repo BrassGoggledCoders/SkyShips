f4747271086c8e98d59de4d74de9bf88162a8141
Skylar <skylar.som@gmail.com>
2023-07-16 19:10:53 -0400

    Make SkyShip dismount slightly nicer
    stolen from Minecart. WIll work on making it smarter

--- src/main/java/xyz/brassgoggledcoders/skyships/block/AeroporteControllerBlock.java
+++ src/main/java/xyz/brassgoggledcoders/skyships/block/AeroporteControllerBlock.java
@@ -5,7 +5,6 @@ import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
---
+++ src/main/java/xyz/brassgoggledcoders/skyships/entity/SkyShipStatus.java
@@ -0,0 +1,9 @@
+package xyz.brassgoggledcoders.skyships.entity;
+
+public enum SkyShipStatus {
+    IN_FLUID,
+    UNDER_FLUID,
+    UNDER_FLOWING_FLUID,
+    ON_LAND,
+    IN_AIR
+}
